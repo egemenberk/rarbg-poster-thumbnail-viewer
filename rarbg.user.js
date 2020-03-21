7b50670306dc06e457169cfddfadbb953448f9be
Egemen Galatali <egemen.galatali@plentific.com>
2020-03-21 14:19:34 +0300

    Format the script automatically

--- rarbg.user.js
+++ rarbg.user.js
@@ -16,30 +16,34 @@ var movieList = null
 if (document.URL.includes("rarbg")) {
    movieList = document.getElementsByClassName("lista2");
 } else {
-    movieList = document.querySelector('#mcol > div > div.block-content > div > div > div > table > tbody').getElementsByTagName("tr");
-    [].slice.call(movieList, 3,movieList.length-1);
+   movieList = document.querySelector(
+         '#mcol > div > div.block-content > div > div > div > table > tbody')
+      .getElementsByTagName("tr");
+   [].slice.call(movieList, 3, movieList.length - 1);
 }
 
 for (i in movieList) {
-    var movie = movieList[i].querySelector('td:nth-child(2) > a:nth-child(1)').getAttribute("onmouseover");
-    var moviePosterUrl = movie.split("\'")[2];
-    var url = moviePosterUrl.substring(0, moviePosterUrl.length -1);
+   var movie = movieList[i].querySelector('td:nth-child(2) > a:nth-child(1)')
+      .getAttribute("onmouseover");
+   var moviePosterUrl = movie.split("\'")[2];
+   var url = moviePosterUrl.substring(0, moviePosterUrl.length - 1);
 
-    var height = "320";
-    var width = "200";
+   var height = "320";
+   var width = "200";
 
-    // For Games
-    if (url.includes("static/over")) {
-        var ar = url.split("static/over");
-        url = ar[0] + "posters2/" + ar[1][1] + ar[1] ;
-    }
-    // For Movies
-    else if (url.includes("mimages")) {
-        url = url.replace("over", "poster");
-    }
-    movieList[i].querySelector('td:nth-child(1) > a > img').setAttribute("src", url);
-    movieList[i].querySelector('td:nth-child(1) > a > img').setAttribute("height",height);
-    movieList[i].querySelector('td:nth-child(1) > a > img').setAttribute("width", width);
+   // For Games
+   if (url.includes("static/over")) {
+      var ar = url.split("static/over");
+      url = ar[0] + "posters2/" + ar[1][1] + ar[1];
+   }
+   // For Movies
+   else if (url.includes("mimages")) {
+      url = url.replace("over", "poster");
+   }
+   movieList[i].querySelector('td:nth-child(1) > a > img').setAttribute("src",
+      url);
+   movieList[i].querySelector('td:nth-child(1) > a > img').setAttribute(
+      "height", height);
+   movieList[i].querySelector('td:nth-child(1) > a > img').setAttribute("width",
+      width);
 }
-
-
